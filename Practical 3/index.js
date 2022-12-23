17c83649c4e634d7f5516d89664599e94ba43bf0
Shubhangkhemani05 <91137484+Shubhangkhemani05@users.noreply.github.com>
2022-12-23 21:33:41 +0530

    .

--- Practical 3/index.js
+++
@@ -1,5 +0,0 @@
-setInterval(
-        function() {
-          var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-          document.body.style.color = "#" + randomColor;
-        }, 1000);
